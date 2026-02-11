bad66e1792ceebed9f344ed83444d8b30702b212
Jakub <dareestijakub@gmail.com>
2026-02-11 16:29:07 +0100

    Improved add_virtual_column function

--- solution.py
+++ solution.py
@@ -7,6 +7,18 @@ def add_virtual_column(df: pd.DataFrame, role: str, new_column: str) -> pd.DataF
     with an additional column calculated based on specified operations.
     """
     
+    df = df.copy()
+
+    #Validation to check column labels consist only of letters and underscore
+    all_columns = list(df.columns) + [new_column]
+
+    for col in all_columns:
+        for char in col:
+            if not (char.isalpha() or char == '_'):
+                return pd.DataFrame()
+
+    #Validation to check that function supports provided operations
+
     avaiables_roles = ["+","-","*"]
 
 
@@ -22,12 +34,11 @@ def add_virtual_column(df: pd.DataFrame, role: str, new_column: str) -> pd.DataF
         return pd.DataFrame()
     
 
-
 #TEST
 
 df_input = pd.DataFrame({'A': [10,20], 'B': [30,40]})
 
-result = add_virtual_column(df_input, "A + B", "C")
+result = add_virtual_column(df_input, "A + B", "C-minor")
 print(result)
 
 
